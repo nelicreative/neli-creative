104e056d93e2f7626fd5c0be10a208689a5ab8c5
Anh Nguyen <anh.nguyen@oivan.com>
2023-07-19 19:39:18 +0700

    Update menu language

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 //This function will be called when user click changing language
 function translate(lng, tagAttr){
-    var translate = new Translate();
-    translate.init(tagAttr, lng);
-    translate.process();
-    if(lng == 'en'){
-      $("#enTranslator").css('color', '#f4623a');
-      $("#viTranslator").css('color', '#212529');
-    } 
-    if(lng == 'kh'){
-      $("#khTranslator").css('color', '#f4623a');
-      $("#viTranslator").css('color', '#212529');
-    }
+  var translate = new Translate();
+  translate.init(tagAttr, lng);
+  translate.process();
+  if(lng == 'en'){
+    $("#navbarDropdownMenuLang").html("<span class='flag-icon flag-icon-us'></span> EN");
+    $("#enTranslator").css('color', '#f4623a');
+    $("#viTranslator").css('color', '#212529');
+  }
+  if(lng == 'vi'){
+    $("#navbarDropdownMenuLang").html("<span class='flag-icon flag-icon-vn'></span> VI");
+    $("#viTranslator").css('color', '#f4623a');
+    $("#viTranslator").css('color', '#212529');
+  }
 }
 $(document).ready(function(){
   //This is id of HTML element (English) with attribute lng-tag
